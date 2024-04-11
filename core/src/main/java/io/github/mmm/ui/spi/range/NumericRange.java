f2db694a732a2a04bc5253ebb2fbb31229ae23c3
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-04-11 22:57:53 +0200

    simplified after mmm-base improvement

--- core/src/main/java/io/github/mmm/ui/spi/range/NumericRange.java
+++ core/src/main/java/io/github/mmm/ui/spi/range/NumericRange.java
@@ -2,12 +2,8 @@
  * http://www.apache.org/licenses/LICENSE-2.0 */
 package io.github.mmm.ui.spi.range;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 import io.github.mmm.base.number.NumberType;
-import io.github.mmm.base.range.AbstractRange;
-import io.github.mmm.base.range.Range;
+import io.github.mmm.base.range.NumberRangeBean;
 import io.github.mmm.base.range.WritableRange;
 import io.github.mmm.validation.Validator;
 
@@ -17,17 +13,11 @@ import io.github.mmm.validation.Validator;
  * @param <V> type of the contained values.
  * @since 1.0.0
  */
-public class NumericRange<V extends Number & Comparable<?>> extends AbstractRange<V> implements WritableRange<V> {
+public class NumericRange<V extends Number & Comparable<?>> extends NumberRangeBean<V> {
 
-  private final NumberType<V> type;
+  private V minFromValidator;
 
-  private V min;
-
-  private V minValidator;
-
-  private V max;
-
-  private V maxValidator;
+  private V maxFromValidator;
 
   /**
    * The constructor.
@@ -36,40 +26,39 @@ public class NumericRange<V extends Number & Comparable<?>> extends AbstractRang
    */
   public NumericRange(NumberType<V> type) {
 
-    super();
-    this.type = type;
+    super(type);
+  }
+
+  /**
+   * The constructor.
+   *
+   * @param type the {@link #getType() number type}.
+   * @param min the {@link #getMin() minimum}.
+   * @param max the {@link #getMax() maximum}.
+   */
+  public NumericRange(NumberType<V> type, V min, V max) {
+
+    super(type, min, max);
   }
 
   @Override
   public V getMin() {
 
-    if (this.min == null) {
-      return this.minValidator;
+    V min = super.getMin();
+    if (min == null) {
+      min = this.minFromValidator;
     }
-    return this.min;
-  }
-
-  @Override
-  public void setMin(V min) {
-
-    this.min = min;
-    onValueChange();
+    return min;
   }
 
   @Override
   public V getMax() {
 
-    if (this.max == null) {
-      return this.maxValidator;
+    V max = super.getMax();
+    if (max == null) {
+      max = this.maxFromValidator;
     }
-    return this.max;
-  }
-
-  @Override
-  public void setMax(V max) {
-
-    this.max = max;
-    onValueChange();
+    return max;
   }
 
   /**
@@ -78,97 +67,19 @@ public class NumericRange<V extends Number & Comparable<?>> extends AbstractRang
   public void setValidator(Validator<? super V> validator) {
 
     if (validator != null) {
-      this.minValidator = this.type.valueOf((Number) validator.getMin());
-      this.maxValidator = this.type.valueOf((Number) validator.getMax());
+      NumberType<V> type = getType();
+      if (type == null) {
+        this.minFromValidator = validator.getMin();
+        this.maxFromValidator = validator.getMax();
+      } else {
+        this.minFromValidator = type.valueOf((Number) validator.getMin());
+        this.maxFromValidator = type.valueOf((Number) validator.getMax());
+      }
     } else {
-      this.minValidator = null;
-      this.maxValidator = null;
+      this.minFromValidator = null;
+      this.maxFromValidator = null;
     }
     onValueChange();
-  }
-
-  /**
-   * @param factor the scaled value in the range from {@code 0} to {@code 1}.
-   * @return the value within this range scaled by the given {@code factor}. It will be {@link #getMin() min} in case
-   *         the given {@code factor} is {@code 0}, while {@code 0} is used in case {@link #getMin() min} is
-   *         {@code null}. In case the {@code factor} is {@code 1}, it will return {@link #getMax() max} or the maximum
-   *         value instead of {@code null}. Any other factor is interpolated between {@link #getMin() min} and
-   *         {@link #getMax() max}.
-   */
-  public V fromFactor(double factor) {
-
-    V minimum = getMin();
-    if (minimum == null) {
-      minimum = this.type.getZero();
-    }
-    if (factor <= 0) {
-      return minimum;
-    }
-    V maximum = getMax();
-    if (maximum == null) {
-      maximum = this.type.getMax();
-      if (maximum == null) {
-        maximum = this.type.valueOf(NumberType.INTEGER.getMax());
-      }
-    }
-    if (factor >= 1) {
-      return maximum;
-    }
-    V delta = this.type.subtract(maximum, minimum);
-    V scaled = this.type.multiply(delta, Double.valueOf(factor));
-    return this.type.add(scaled, minimum);
-  }
-
-  /**
-   * @param value the value within this range.
-   * @return the scaled value. It will be {@link #getMin() min} in case the given {@code factor} is {@code 0}, while
-   *         {@code 0} is used in case {@link #getMin() min} is {@code null}. In case the {@code factor} is {@code 1},
-   *         it will return {@link #getMax() max} or the maximum value instead of {@code null}. Any other factor is
-   *         interpolated between {@link #getMin() min} and {@link #getMax() max}.
-   */
-  public double toFactor(V value) {
-
-    V minimum = getMin();
-    if (minimum == null) {
-      minimum = this.type.getZero();
-    }
-    V maximum = getMax();
-    if (maximum == null) {
-      maximum = this.type.getMax();
-      if (maximum == null) {
-        maximum = this.type.valueOf(NumberType.INTEGER.getMax());
-      }
-    }
-    V delta = this.type.subtract(maximum, minimum);
-    V offset = this.type.subtract(value, minimum);
-    if (this.type.isDecimal()) {
-      return this.type.divide(offset, delta).doubleValue();
-    } else if (this.type.getExactness() == NumberType.BIG_INTEGER.getExactness()) {
-      return new BigDecimal((BigInteger) offset).divide(new BigDecimal((BigInteger) delta)).doubleValue();
-    } else {
-      return offset.doubleValue() / delta.doubleValue();
-    }
-  }
-
-  /**
-   * Called whenever {@link #getMin() min} or {@link #getMax() max} changes.
-   */
-  protected void onValueChange() {
-
-  }
-
-  @Override
-  public Range<V> withMin(V minimum) {
-
-    setMin(minimum);
-    return this;
-  }
-
-  @Override
-  public Range<V> withMax(V maximum) {
-
-    setMax(maximum);
-    return this;
   }
 
 }
