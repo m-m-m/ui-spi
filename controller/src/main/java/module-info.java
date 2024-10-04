ff627c7ce9a49d5df1df487630fd3548010ede42
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-10-04 12:47:55 +0200

    JavaDoc

--- controller/src/main/java/module-info.java
+++ controller/src/main/java/module-info.java
@@ -3,8 +3,8 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 /**
- * Provides SPI for controller management and navigation.
- * 
+ * Provides service provider interface (SPI) for controller management and navigation.
+ *
  * @provides io.github.mmm.ui.api.controller.UiController
  * @uses io.github.mmm.ui.api.controller.UiController
  */
